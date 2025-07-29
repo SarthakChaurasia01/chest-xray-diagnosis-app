3a4f063a2f3f653aa1ae94fb836a0ec035ad5096
Sarthak Chaurasia <166941942+SarthakChaurasia01@users.noreply.github.com>
2025-07-29 10:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,41 +2,25 @@ import gradio as gr
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from tensorflow.keras.layers import InputLayer
 
-# Custom InputLayer to handle batch_shape
-class CustomInputLayer(InputLayer):
-    @classmethod
-    def from_config(cls, config):
-        if "batch_shape" in config:
-            config["batch_input_shape"] = config.pop("batch_shape")  # Convert to compatible key
-        return cls(**config)
+model = tf.keras.models.load_model("Chest_Xray_Model.h5")
 
-# Load the model with custom objects
-model = tf.keras.models.load_model("model.h5", custom_objects={"InputLayer": CustomInputLayer})
+class_names = ['Normal', 'COVID-19', 'Pneumonia']
 
-# Preprocess function
-def preprocess(image):
-    image = image.resize((224, 224))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    return image
-
-# Prediction function
 def predict(image):
-    image = preprocess(image)
-    predictions = model.predict(image)[0]
-    classes = ["Normal", "COVID-19", "Pneumonia"]
-    results = {cls: float(predictions[i]) for i, cls in enumerate(classes)}
-    return results
+    image = image.resize((224, 224))
+    image = np.expand_dims(image, axis=0)
+    image = image / 255.0
+    prediction = model.predict(image)[0]
+    return {class_names[i]: float(prediction[i]) for i in range(len(class_names))}
 
-# Create interface
-demo = gr.Interface(
+interface = gr.Interface(
     fn=predict,
     inputs=gr.Image(type="pil"),
     outputs=gr.Label(num_top_classes=3),
-    title="Chest X-Ray Classifier",
-    description="Upload a chest X-ray image. The model will classify it as Normal, COVID-19, or Pneumonia."
+    title="Chest X-Ray Disease Classifier",
+    description="Upload an X-ray image to detect Normal, COVID-19, or Pneumonia."
 )
 
-demo.launch()
+if __name__ == "__main__":
+    interface.launch()
