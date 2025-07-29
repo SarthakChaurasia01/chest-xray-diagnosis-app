2635a62f844efb1de3b31d7b9c2e603ac9aa3ea7
Sarthak Chaurasia <166941942+SarthakChaurasia01@users.noreply.github.com>
2025-07-29 10:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,15 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-model = tf.keras.models.load_model("Chest_Xray_Model.h5")
+model = tf.keras.models.load_model("Chest_X-Ray_Classification_Model.h5")
+labels = ["Normal", "COVID-19", "Pneumonia"]
 
-class_names = ['Normal', 'COVID-19', 'Pneumonia']
+def classify(img):
+    img = img.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    preds = model.predict(img_array)[0]
+    return {labels[i]: float(preds[i]) for i in range(len(labels))}
 
-def predict(image):
-    image = image.resize((224, 224))
-    image = np.expand_dims(image, axis=0)
-    image = image / 255.0
-    prediction = model.predict(image)[0]
-    return {class_names[i]: float(prediction[i]) for i in range(len(class_names))}
-
-interface = gr.Interface(
-    fn=predict,
-    inputs=gr.Image(type="pil"),
-    outputs=gr.Label(num_top_classes=3),
-    title="Chest X-Ray Disease Classifier",
-    description="Upload an X-ray image to detect Normal, COVID-19, or Pneumonia."
-)
-
-if __name__ == "__main__":
-    interface.launch()
+demo = gr.Interface(fn=classify, inputs=gr.Image(type="pil"), outputs=gr.Label())
+demo.launch(server_name="0.0.0.0", server_port=8080)
