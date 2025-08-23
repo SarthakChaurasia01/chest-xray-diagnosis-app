e60b48c7addb2b3f52f372f03f7e0a98a5c592f8
Sarthak Chaurasia <166941942+SarthakChaurasia01@users.noreply.github.com>
2025-08-23 18:51:34 +0530

    Create model.py

---
+++ utils/model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+class ChestXRayClassifier(nn.Module):
+    def __init__(self, input_shape, output_shape, hidden_units):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units*51*51, output_shape)
+        )
+
+    def forward(self, x):
+        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
